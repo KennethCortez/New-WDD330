bec367d3c65d7920d421b9bbce46b50d422977df
KennethCortez <kennethleocortez@gmail.com>
2025-09-14 12:56:44 -0600

    Make the changes of week 1 activitie

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,15 @@
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { setLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import getParam from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam('product');
 
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {

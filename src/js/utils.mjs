0c2ac80ee08c1070fade5b3facdbe94a1f3c2652
KennethCortez <kennethleocortez@gmail.com>
2025-09-18 19:34:30 -0600

    changes in the utils

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -57,6 +57,6 @@ export async function loadHeaderFooter(){
   const footerTemplate = await loadTemplate("../partials/footer.html");
   const footerElement = document.querySelector("#main-footer");
 
-  renderWithTemplate(headerTemplate, headerElement, {}, () => {});
-  renderWithTemplate(footerTemplate, footerElement, {}, () => {});
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
 }
